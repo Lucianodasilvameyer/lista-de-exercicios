1e6246d631ba6eae65399457f64e27db75c3670a
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-02 17:47:23 -0300

    estudo no exercício 2

---
+++ exercicio 2 da revisão/exercicio 2 da revisão/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio_2_da_revisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
