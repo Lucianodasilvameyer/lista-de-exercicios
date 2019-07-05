eabbe1baede89197e35e02c360fc8b5e96f5f98b
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-05 12:40:14 -0300

    exercicio 1 e 2

---
+++ Exercicio2Peso/Exercicio2Peso/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2Peso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double peso = -1 ;
+            int idade = 0;
+            int quantidadeHonmens = 0, quantidadeMulheres = 0, homens18=0;
+            string sexo = "";
+
+            while( peso != 0)
+            {
+                Console.WriteLine("Digite peso:");
+                peso = Convert.ToDouble(Console.ReadLine());
+
+                if (peso == 0) break;
+
+                Console.WriteLine("Digite Idade:");
+                idade = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Digite Sexo (M/F):");
+
+                sexo = Console.ReadLine(); 
+                sexo = sexo.ToLower();//o ToLower serve para converter para minusculo
+
+
+                if(sexo == "m" || sexo == "masculino" )
+                {
+                    quantidadeHonmens++;
+
+                    if(idade>18)
+                    {
+                        homens18++;
+                        
+                    }
+                }
+                if(sexo=="f"||sexo=="feminino")
+                {
+                    quantidadeMulheres++;
+                }
+
+                
+                
+
+                
+
+            }
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Quantidade de Homens: " + quantidadeHonmens);
+
+            Console.WriteLine("o numero de homens com mais de 18 anos é: " + homens18);
+
+            Console.WriteLine("Quantidade de mulheres: " + quantidadeMulheres);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void semArray()
+        {
+            int num; //como quer saber quantos são negativos pode usar a variavel num, se fosse quais numeros ai sim pode colocar num1,num1,nmu3... 
+            int caixa=0;
+            
+
+            Console.WriteLine("digite 5 valores para saber quantos são negativos");
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num < 0) caixa++;
+
+                
+
+               
+            }
+            Console.WriteLine("tem: " + caixa + "numeros negativos");
+
+
+        }
+
+        static void comArray()
+        {
+            int[] numeros = new int[5];
+            int Sacola=0;
+
+            Console.WriteLine("digite 5 numeros para saber quantos são negativos");
+             
+            for(int i=0; i <numeros.Length; i++)
+            {
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (numeros[i] < 0) Sacola++;
+            }
+            Console.WriteLine("existem " + Sacola + " numeros negativos. Os numeros negativos digitados são: ");
+
+            for(int i=0; i < numeros.Length;i++)
+            {
+                if(numeros[i]<0)
+                {
+                    Console.WriteLine(numeros[i]);
+                }
+            }    
+
+            Console.ReadKey();
+        }
+
+        // fazer um programa que o usuario digite idade, sexo e peso de uma pessoa
+        // enquanto  não digitar uma idade com valor 0, ficae lendo
+
+        // mostrar quantos homens
+        // quantas mulheres
+        // quantos homens com idade > 18
+        // quantas mulheres com > 30 e peso menor 70
+        // mostrar media de peso geral
+        // media de idade das mulheres
+
+        static void Main(string[] args)
+        {
+
+
+            //semArray();
+            comArray();
+
+            Console.ReadKey();
+
+        }
+    }
+}
